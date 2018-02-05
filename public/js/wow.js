31c22dfd974c21c9467a10f93fe97122250d7687
kirstystroud <kirstystroud@btinternet.com>
2018-02-05 22:33:10 +0000

    Refactored pie chart drawing in stats.js

--- public/js/wow.js
+++ public/js/wow.js
@@ -723,97 +723,79 @@ var drawChart = function(respData) {
  * Build kills pie chart from data
  */
 var drawPieChart = function(respData) {
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawPieChartInternal);
 
-    function drawPieChartInternal() {
+    var rawData = [];
+    rawData.push(['Class', 'Total Kills']);
+    $.each(respData, function(k, v) {
+        rawData.push([v.name, parseInt(v.kills)]);
+    });
 
-        var colorMap = {
-            1 : '#c69b6d', // Warrior
-            2 : '#f48cba', // Paladin
-            3 : '#aad372', // Hunter
-            4 : '#fff468', // Rogue
-            5 : 'white', // Priest
-            6 : '#c41e3b', // Death Knight
-            7 : '#2359ff', // Shaman
-            8 : '#68ccef', // Mage
-            9 : '#9382c9', // Warlock
-            10 : '#00ffba', // Monk
-            11 : '#ff7c0a', // Druid
-            12 : '#a330c9', // Demon Hunter
-        };
+    var sliceData = buildSliceData(respData);
 
-        var rawData = [];
-        var sliceData = {};
-        rawData.push(['Class', 'Total Kills']);
-        $.each(respData, function(k, v) {
-            rawData.push([v.name, parseInt(v.kills)]);
-            sliceData[k] = { 'color' : colorMap[ v['id_ext'] ] };
-        });
-
-        var data = google.visualization.arrayToDataTable(rawData);
-
-        var options = {
-            title: 'Kills by Class',
-            titlePosition : 'out',
-            titleTextStyle : {
-                color : '#f5eBd1',
-                fontSize : 18
-            },
-            backgroundColor : {
-                stroke : '#f5eBd1',
-                fill : '#231207'
-            },
-            slices : sliceData,
-            legend : {
-                textStyle : {
-                    color : '#f5eBd1'
-                }
-            },
-            tooltip : {
-                isHtml : true
-            }
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('stats-pie-div'));
-        chart.draw(data, options);
-    };
+    drawPieChartFromConfig(rawData, sliceData, { title : 'Kills by Class', div : 'stats-pie-div' });
 };
 
+/**
+ * Build quests pie chart from data
+ */
 var drawQuestsPieChart = function(respData) {
 
+    var rawData = [];
+    rawData.push(['Class', 'Quests Completed']);
+    $.each(respData, function(k, v) {
+        rawData.push([v.name, parseInt(v.quests)]);
+    });
+
+    var sliceData = buildSliceData(respData);
+
+    drawPieChartFromConfig(rawData, sliceData, { title : 'Quests by Class', div : 'stats-pie-quests-div' });
+};
+
+/**
+ * Build slice data from response data
+ * @param {Object} respData
+ * @return {Object}
+ */
+var buildSliceData = function(respData) {
+    var colorMap = {
+        1 : '#c69b6d', // Warrior
+        2 : '#f48cba', // Paladin
+        3 : '#aad372', // Hunter
+        4 : '#fff468', // Rogue
+        5 : 'white', // Priest
+        6 : '#c41e3b', // Death Knight
+        7 : '#2359ff', // Shaman
+        8 : '#68ccef', // Mage
+        9 : '#9382c9', // Warlock
+        10 : '#00ffba', // Monk
+        11 : '#ff7c0a', // Druid
+        12 : '#a330c9', // Demon Hunter
+    };
+
+    var sliceData = {};
+    $.each(respData, function(k, v) {
+        sliceData[k] = { 'color' : colorMap[ v['id_ext'] ] };
+    });
+
+    return sliceData;
+};
+
+/**
+ * Build generic pie chart from config
+ * @param {Object} rawData
+ * @param {Object} sliceData
+ * @param {Object} config - required keys 'title' , 'div'
+ */
+var drawPieChartFromConfig = function(rawData, sliceData, config) {
+
     google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawQuestsPieChartInternal);
+    google.charts.setOnLoadCallback(drawPieChartFromConfigInternal);
 
-    function drawQuestsPieChartInternal() {
-
-        var colorMap = {
-            1 : '#c69b6d', // Warrior
-            2 : '#f48cba', // Paladin
-            3 : '#aad372', // Hunter
-            4 : '#fff468', // Rogue
-            5 : 'white', // Priest
-            6 : '#c41e3b', // Death Knight
-            7 : '#2359ff', // Shaman
-            8 : '#68ccef', // Mage
-            9 : '#9382c9', // Warlock
-            10 : '#00ffba', // Monk
-            11 : '#ff7c0a', // Druid
-            12 : '#a330c9', // Demon Hunter
-        };
-
-        var rawData = [];
-        var sliceData = {};
-        rawData.push(['Class', 'Quests Completed']);
-        $.each(respData, function(k, v) {
-            rawData.push([v.name, parseInt(v.quests)]);
-            sliceData[k] = { 'color' : colorMap[ v['id_ext'] ] };
-        });
-
+    function drawPieChartFromConfigInternal() {
         var data = google.visualization.arrayToDataTable(rawData);
 
         var options = {
-            title: 'Quests Completed by Class',
+            title: config.title,
             titlePosition : 'out',
             titleTextStyle : {
                 color : '#f5eBd1',
@@ -834,10 +816,9 @@ var drawQuestsPieChart = function(respData) {
             }
         };
 
-        var chart = new google.visualization.PieChart(document.getElementById('stats-pie-quests-div'));
+        var chart = new google.visualization.PieChart(document.getElementById(config.div));
         chart.draw(data, options);
-    };
-
+    }; 
 };
 
 $(document).ready(function() {
