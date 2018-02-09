f5d45093bb5a71c1ff13891fe3c1cb76ffb25f76
kirstystroud <kirstystroud@btinternet.com>
2018-02-09 21:23:30 +0000

    Refactored quests hs

--- resources/assets/js/quests.js
+++ resources/assets/js/quests.js
@@ -1,79 +1,101 @@
 $(document).ready(function() {
     if (window.location.pathname == '/quests') {
-        setupQuestSearch();
+        var qh = new QuestHandler().init();
     }
 });
 
-var setupQuestSearch = function() {
-    // Quests searching
-    $('#wow-button-submit-quests').click(function(event) {
-        event.preventDefault();
-        $('#quest-results').empty();
-        $('#quest-results').append('<br>', '<p class="wow-searching">Searching...</p>');
 
-        var data = {
-            character : $('#quests-characters-select').val(),
-            category : $('#quests-categories-select').val()
-        };
-        $.ajax({
-            url : '/quests/search',
-            data : data,
-            method : 'GET',
-            success : function(resp) {
-                $('#quest-results').empty();
-                $('#quest-results').append(resp);
 
-                // Attach event handlers to links
-                $('.td-category').click(function(event) {
-                    event.preventDefault();
-                    $('#quests-categories-select').val($(this).attr('category-id'));
-                    $('#quests-characters-select').val($(this).attr('character-id'));
-                    $('#wow-button-submit-quests').click();
-                });
 
-                attachQuestsEventHandlers();
-            },
-            error : function(err) {
-                console.log(err);
-            }
+/**
+ * Class to handle all js functionality on quests page
+ */
+function QuestHandler() {
+
+    /**
+     * Public entry point
+     */
+    this.init = function() {
+        setupQuestSearch();
+    };
+
+    /**
+     * Initialise searching and process response
+     */
+    var setupQuestSearch = function() {
+        // Quests searching
+        $('#wow-button-submit-quests').click(function(event) {
+            event.preventDefault();
+            $('#quest-results').empty();
+            $('#quest-results').append('<br>', '<p class="wow-searching">Searching...</p>');
+
+            var data = {
+                character : $('#quests-characters-select').val(),
+                category : $('#quests-categories-select').val()
+            };
+            $.ajax({
+                url : '/quests/search',
+                data : data,
+                method : 'GET',
+                success : function(resp) {
+                    $('#quest-results').empty();
+                    $('#quest-results').append(resp);
+
+                    // Attach event handlers to links
+                    $('.td-category').click(function(event) {
+                        event.preventDefault();
+                        $('#quests-categories-select').val($(this).attr('category-id'));
+                        $('#quests-characters-select').val($(this).attr('character-id'));
+                        $('#wow-button-submit-quests').click();
+                    });
+
+                    attachQuestsEventHandlers();
+                },
+                error : function(err) {
+                    console.log(err);
+                }
+            });
         });
-    });
-};
+    };
 
-var attachQuestsEventHandlers = function() {
-    // Quests character compare
-    $('#wow-button-submit-quests-compare').click(function(event) {
-        event.preventDefault();
-        var data = {
-            character : $('#quests-characters-select').val(),
-            category : $('#quests-categories-select').val(),
-            compare : $('#quests-compare-characters-select').val()
-        };
+    /**
+     * Attach required event handlers
+     */
+    var attachQuestsEventHandlers = function() {
+        // Quests character compare
+        $('#wow-button-submit-quests-compare').click(function(event) {
+            event.preventDefault();
+            var data = {
+                character : $('#quests-characters-select').val(),
+                category : $('#quests-categories-select').val(),
+                compare : $('#quests-compare-characters-select').val()
+            };
 
-        $('#quest-results').empty();
-        $('#quest-results').append('<br>', '<p class="wow-searching">Searching...</p>');
+            $('#quest-results').empty();
+            $('#quest-results').append('<br>', '<p class="wow-searching">Searching...</p>');
 
-        $.ajax({
-            url : '/quests/search',
-            data : data,
-            method : 'GET',
-            success : function(resp) {
-                $('#quest-results').empty();
-                $('#quest-results').append(resp);
+            $.ajax({
+                url : '/quests/search',
+                data : data,
+                method : 'GET',
+                success : function(resp) {
+                    $('#quest-results').empty();
+                    $('#quest-results').append(resp);
 
-                // Attach event handlers to links
-                $('.td-category').click(function(event) {
-                    event.preventDefault();
-                    $('#quests-categories-select').val($(this).attr('category-id'));
-                    $('#quests-characters-select').val($(this).attr('character-id'));
-                    $('#wow-button-submit-quests').click();
-                });
+                    // Attach event handlers to links
+                    $('.td-category').click(function(event) {
+                        event.preventDefault();
+                        $('#quests-categories-select').val($(this).attr('category-id'));
+                        $('#quests-characters-select').val($(this).attr('character-id'));
+                        $('#wow-button-submit-quests').click();
+                    });
 
-                attachQuestsEventHandlers();
-            },
-            error : function(err) {
-                console.log(err);
-            }
+                    attachQuestsEventHandlers();
+                },
+                error : function(err) {
+                    console.log(err);
+                }
+            });
         });
-    });
+    };
 };
