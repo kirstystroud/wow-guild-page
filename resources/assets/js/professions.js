b38351e7a9bb442ef65a449a65d8e62e1dd8aad6
kirstystroud <kirstystroud@btinternet.com>
2018-02-09 21:19:57 +0000

    Refactored professions js

--- resources/assets/js/professions.js
+++ resources/assets/js/professions.js
@@ -1,31 +1,47 @@
 $(document).ready(function() {
     if (window.location.pathname == '/professions') {
-        attachProfessionsEventHandlers();
+        var ph = new ProfessionsHandler().init();
     }
 });
 
-var attachProfessionsEventHandlers = function() {
-    // Submitting profession search form
-    $('#wow-button-submit').click(function(event) {
-        event.preventDefault();
-        $('#search-recipes-result').empty();
-        $('#search-recipes-result').append('<br>', '<p class="wow-searching">Searching...</p>');
+/**
+ * Class for handling all js functionality on professions page
+ */
+function ProfessionsHandler() {
 
-        var formData = {
-            name : $('#name').val(),
-            profession : $('#profession').val()
-        };
-        $.ajax({
-            url : '/professions/search',
-            data : formData,
-            method : 'GET',
-            success : function(resp) {
-                $('#search-recipes-result').empty();
-                $('#search-recipes-result').append(resp);
-            },
-            error : function(err) {
-                console.log(err);
-            }
+    /**
+     * Public entry point
+     */
+    this.init = function() {
+        attachProfessionsEventHandlers();
+    };
+
+    /**
+     * Attach required event handlers
+     */
+    var attachProfessionsEventHandlers = function() {
+        // Submitting profession search form
+        $('#wow-button-submit').click(function(event) {
+            event.preventDefault();
+            $('#search-recipes-result').empty();
+            $('#search-recipes-result').append('<br>', '<p class="wow-searching">Searching...</p>');
+
+            var formData = {
+                name : $('#name').val(),
+                profession : $('#profession').val()
+            };
+            $.ajax({
+                url : '/professions/search',
+                data : formData,
+                method : 'GET',
+                success : function(resp) {
+                    $('#search-recipes-result').empty();
+                    $('#search-recipes-result').append(resp);
+                },
+                error : function(err) {
+                    console.log(err);
+                }
+            });
         });
-    });
-};
+    };
+}
